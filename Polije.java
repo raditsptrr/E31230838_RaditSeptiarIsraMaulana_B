ba11d2ae2bab7be3cc1a4cad90bd204c96ad3ab6
raditsptrr <158048437+raditsptrr@users.noreply.github.com>
2024-01-30 11:21:38 +0700

    Create Polije.java

---
+++ Polije.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package polije;
+public class Polije {
+    public static void main(String[] args) {
+        System.out.println("Politeknik Negeri Jember"); 
+    } 
+}
